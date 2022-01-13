f0d2db117c6f2fc107c57693b52bac6be9809049
Vaibhav Singh <vaibhav.singh@PS97.Team>
2022-01-13 11:35:04 +0530

    Added Manifest and service worker

---
+++ PWAProject/wwwroot/ServiceWorker.js
@@ -0,0 +1,34 @@
+﻿const casheName = "integer-byte -site-v1";
+const assets = [
+    "/"
+];
+
+self.addEventListener("install", installEvent => {
+    console.log("test3");
+    installEvent.waitUntil(
+        caches.open(casheName).then(cache => {
+            cache.addAll(assets)
+        })
+         
+    )
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== assets)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+});
